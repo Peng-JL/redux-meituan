be998fc66e6c9ad19223c5e4588a0d10d6caf49d
pjl <1007020896@qq.com>
2025-12-18 17:29:16 +0800

    添加购物车实现

--- src/store/modules/takeaway.js
+++ src/store/modules/takeaway.js
@@ -6,7 +6,8 @@ const foodsStore = createSlice({
     name: "foods",
     initialState: {
         foodsList: [],
-        activeIndex: 0
+        activeIndex: 0,
+        cartList: []
     },
     reducers: {
         setFoodsList: (state, action) => {
@@ -14,11 +15,19 @@ const foodsStore = createSlice({
         },
         changeActiveIndex: (state, action) => {
             state.activeIndex = action.payload;
+        },
+        addCart(state, action) {
+            const item = state.cartList.find(item => item.id === action.payload.id)
+            if (item) {
+                item.count++
+            } else {
+                state.cartList.push(action.payload)
+            }
         }
     },
 })
 
-const { setFoodsList, changeActiveIndex } = foodsStore.actions;
+const { setFoodsList, changeActiveIndex, addCart } = foodsStore.actions;
 const fetchFoodsList = () => {
     return async (dispatch) => {
         const res = await axios.get("http://localhost:3004/takeaway");
@@ -27,6 +36,6 @@ const fetchFoodsList = () => {
     }
 }
 
-export { fetchFoodsList, changeActiveIndex }
+export { fetchFoodsList, changeActiveIndex, addCart }
 const reducer = foodsStore.reducer;
 export default reducer;
